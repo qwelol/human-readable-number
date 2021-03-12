ebccc06eb1582c69ef572dbdf597fc21a9ad6c65
Alex <argentum.tm@gmail.com>
2021-03-12 20:00:37 +0300

    feat: implement toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,71 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let numbers = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    let teens = [
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    let dozens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    let result = "";
+    let n = number;
+    let counter = 1;
+    if (n === 0 ) {
+        result = numbers[n];
+    }
+    while (n > 0) {
+        switch (counter) {
+            case 1: {
+                if (n % 10 > 0) {
+                    result = numbers[n % 10];
+                }
+                break;
+            }
+            case 2: {
+                if (n % 10 === 1) {
+                    result = teens[number % 10] + " ";
+                } else {
+                    result = dozens[n % 10] + " " + result;
+                }
+                break;
+            }
+            case 3: {
+                result = numbers[n % 10] + " hundred " + result;
+                break;
+            }
+        }
+        n = (n - (n % 10)) / 10;
+        counter++;
+        result = result.trim();
+    }
+    return result;
+};
